1fac4d1e020ebc8918ac8a953eb089b90e1acc5a
Black-Garlic <hn1226@naver.com>
2023-04-02 20:41:20 +0900

    영웅 목록 필터 검색

--- src/main/java/com/guardian/tales/repository/HeroInfoRepositoryImpl.java
+++ src/main/java/com/guardian/tales/repository/HeroInfoRepositoryImpl.java
@@ -1,9 +1,6 @@
 package com.guardian.tales.repository;
 
-import com.guardian.tales.domain.Hero;
-import com.guardian.tales.domain.HeroInfo;
-import com.guardian.tales.domain.QHero;
-import com.guardian.tales.domain.QHeroInfo;
+import com.guardian.tales.domain.*;
 import com.guardian.tales.web.rest.vm.HeroVM;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
@@ -23,12 +20,23 @@ public class HeroInfoRepositoryImpl extends QuerydslRepositorySupport implements
     public List<HeroInfo> findByQDSLSearchValues(HeroVM.ListParam param) {
         QHeroInfo heroInfo = QHeroInfo.heroInfo;
         QHero hero = QHero.hero;
+        QRole role = QRole.role;
+        QElement element = QElement.element;
+        QWeapon weapon = QWeapon.weapon;
+        QWeaponType weaponType = QWeaponType.weaponType;
+        QChainType chainType = QChainType.chainType;
 
         JPQLQuery<HeroInfo> query = from(heroInfo);
         query.leftJoin(hero).on(heroInfo.hero.eq(hero));
+        query.leftJoin(role).on(hero.role.eq(role));
+        query.leftJoin(element).on(hero.element.eq(element));
+        query.leftJoin(weapon).on(heroInfo.weapon.eq(weapon));
+        query.leftJoin(weaponType).on(weapon.weaponType.eq(weaponType));
+        query.leftJoin(chainType).on(weapon.chainType.eq(chainType));
 
         BooleanBuilder andBuilder = new BooleanBuilder();
 
+        // 키워드 검색
         if (param.getKeyword() != null && !param.getKeyword().isEmpty()) {
             BooleanBuilder orBuilder = new BooleanBuilder();
 
@@ -36,6 +44,94 @@ public class HeroInfoRepositoryImpl extends QuerydslRepositorySupport implements
 
             andBuilder.and(orBuilder);
         }
+
+        // 등급 필터
+        if (param.getStage() != null && param.getStage().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String stageName : param.getStage()) {
+                orBuilder.or(heroInfo.hero.stage.eq(stageName));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        // 역할 필터
+        if (param.getRole() != null && param.getRole().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String roleName : param.getRole()) {
+                orBuilder.or(heroInfo.hero.role.name.eq(roleName));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        // 속성 필터
+        if (param.getElement() != null && param.getElement().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String elementName : param.getElement()) {
+                orBuilder.or(heroInfo.hero.element.name.eq(elementName));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        // 무기 종류 필터
+        if (param.getWeaponType() != null && param.getWeaponType().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String weaponTypeName : param.getWeaponType()) {
+                orBuilder.or(heroInfo.weapon.weaponType.name.eq(weaponTypeName));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        // 무기 스킬 타입 필터
+        if (param.getWeaponSkillChainType() != null && param.getWeaponSkillChainType().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String weaponSkillChainTypeName : param.getWeaponSkillChainType()) {
+                orBuilder.or(heroInfo.weapon.chainType.name.eq(weaponSkillChainTypeName));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        // 파티 버프
+        if (param.getPartyBuff() != null && param.getPartyBuff().size() > 0) {
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            for (String weaponSkillChainTypeName : param.getPartyBuff()) {
+                orBuilder.or(heroInfo.partyBuff.like("%" + weaponSkillChainTypeName + "%", '!'));
+            }
+
+            andBuilder.and(orBuilder);
+        }
+
+        //        // 연계기 시작 타입 필터
+        //        if (param.getStartChainType() != null && param.getStartChainType().size() > 0) {
+        //            BooleanBuilder orBuilder = new BooleanBuilder();
+        //
+        //            for (String  : param.getStartChainType()) {
+        //                orBuilder.or(heroInfo.hero.stage.eq(stage));
+        //            }
+        //
+        //            andBuilder.and(orBuilder);
+        //        }
+        //
+        //        // 연계기 종료 타입 필터
+        //        if (param.getStage() != null && param.getStage().size() > 0) {
+        //            BooleanBuilder orBuilder = new BooleanBuilder();
+        //
+        //            for (String stage : param.getStage()) {
+        //                orBuilder.or(heroInfo.hero.stage.eq(stage));
+        //            }
+        //
+        //            andBuilder.and(orBuilder);
+        //        }
 
         query.where(andBuilder);
         query.distinct();
--- src/main/java/com/guardian/tales/web/rest/vm/HeroVM.java
+++ src/main/java/com/guardian/tales/web/rest/vm/HeroVM.java
@@ -1,5 +1,6 @@
 package com.guardian.tales.web.rest.vm;
 
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +11,13 @@ public class HeroVM {
     public static class ListParam {
 
         private String keyword;
+        private Set<String> stage;
+        private Set<String> role;
+        private Set<String> element;
+        private Set<String> weaponType;
+        private Set<String> weaponSkillChainType;
+        private Set<String> partyBuff;
+        private Set<String> startChainType;
+        private Set<String> endChainType;
     }
 }
